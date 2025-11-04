4622e26e5e05e79496b5b92edf9e41c6ac607f67
therron-tyler <therrontyler@gmail.com>
2025-11-04 10:43:10 -0600

    Update sbatch_merge_fastq.sh

--- sbatch_merge_fastq.sh
+++ sbatch_merge_fastq.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-#SBATCH -A b1042
+#SBATCH -A alloc
 #SBATCH -p genomics
 #SBATCH -t 40:00:00
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=tyler.therron@northwestern.edu
+#SBATCH --mail-user=email
 #SBATCH --output=%x.%j.out
 #SBATCH --mem=60gb
 #SBATCH --job-name=snp
 #SBATCH -N 1
 #SBATCH -n 12
 
-sample=RS1_S1
+sample=sample
 cat $(ls -1 ${sample}_L*_R1_001.fastq.gz | sort -V) > ${sample}_R1.fastq.gz
 cat $(ls -1 ${sample}_L*_R2_001.fastq.gz | sort -V) > ${sample}_R2.fastq.gz
 
-echo "R1 merged lines:" $(gzip -cd RS1_S1_R1.fastq.gz | wc -l)
-echo "R2 merged lines:" $(gzip -cd RS1_S1_R2.fastq.gz | wc -l)
+echo "R1 merged lines:" $(gzip -cd sample_S1_R1.fastq.gz | wc -l)
+echo "R2 merged lines:" $(gzip -cd sample_S1_R2.fastq.gz | wc -l)
 
